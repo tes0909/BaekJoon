c875ce0cfaeac7a5dbc2a8cdf417411d152fd65c
tes0909 <krosa1213@gmail.com>
2025-07-10 19:09:30 +0900

    [Bronze V] Title: 꼬마 정민, Time: 48 ms, Memory: 5480 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,5 @@
+string[] numbers = Console.ReadLine().Split(" ");
+long A = long.Parse(numbers[0]);
+long B = long.Parse(numbers[1]);
+long C = long.Parse(numbers[2]);
+Console.WriteLine(A + B + C);
