8d23836d54507f1410f06f19a64f50f0ad694688
tes0909 <krosa1213@gmail.com>
2025-09-03 00:23:40 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 320 ms, Memory: 8036 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cs
@@ -0,0 +1,33 @@
+int[] numbers = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int N = numbers[0];
+int M = numbers[1];
+
+int[,] result = new int[N, M];
+
+for (int i = 0; i < N; i++)
+{
+    int[] row = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < M; j++)
+    {
+        result[i, j] += row[j];
+    }
+}
+
+for (int i = 0; i < N; i++)
+{
+    int[] row = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    for (int j = 0; j < M; j++)
+    {
+        result[i, j] += row[j];
+    }
+}
+
+for (int i = 0; i < N; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        Console.Write(result[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
